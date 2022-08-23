c6ba568dba7db3e79cc5b6bdcac7adefa1427b3c
Dong Keun LIM <86710265+Limdongkeun@users.noreply.github.com>
2022-08-23 11:28:12 +0900

    Add files via upload

---
+++ Backend/StackOverFlow/src/main/java/com/Team22/preproject/StackOverFlow/StackOverFlowApplication.java
@@ -0,0 +1,13 @@
+package com.Team22.preproject.StackOverFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackOverFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackOverFlowApplication.class, args);
+	}
+
+}
