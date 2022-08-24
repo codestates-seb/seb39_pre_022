169f4105f1fc92877f275d1b71c9bfdbbdefbd70
Dong Keun LIM <86710265+Limdongkeun@users.noreply.github.com>
2022-08-24 19:24:51 +0900

    test

---
+++ Backend/StackOverFlow/src/main/java/com/Team22/preproject/StackOverFlow/controller.java
@@ -0,0 +1,14 @@
+package com.Team22.preproject.StackOverFlow;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+    @GetMapping("/")
+    public String helloWorld(){
+        return "To-do Application!!";
+    }
+}
