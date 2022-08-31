259b1c21fa1990795a60601a42385392b9cb4a05
최지혜 <100934753+ji-hyeee@users.noreply.github.com>
2022-08-31 10:41:44 +0900

    make answer component

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './component/Main';
+import Login from './component/LogIn';
+import SignUp from './component/SignUp';
+import Question from './component/Question';
+import Questions from './component/Questions';
+import AskQuestion from './component/AskQuestion';
+
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/questions' element={<Questions />} />
+          <Route path='/question' element={<Question />} />
+          <Route path='/askquestion' element={<AskQuestion />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+        </Routes>
+      </div >
+    </BrowserRouter>
+  );
+}
+
+
+// 1rem === 12-3px
+// 세밀하게 작업할 때는 .단위
