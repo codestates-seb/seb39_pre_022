5eddc141ffafd3a6a13ebbbd5aa329bb889db386
SOOOIN <strong0bunny@gmail.com>
2022-08-31 13:27:27 +0900

    검색창 반응형으로 수정

--- Frontend/project/src/styles/Navbar_style.js
+++ Frontend/project/src/styles/Navbar_style.js
@@ -9,31 +9,35 @@ export const NavbarContainer = styled.nav`
   @media (min-width: 700px) {
     height: 80px;
   }
-`;
+  `;
 
 export const LeftContainer = styled.div`
   flex: 80%;
   display: flex;
   align-items: center;
   padding-left: 5%;
-`;
+  .inputContainer{
+    background-color: aqua;
+    width: 100%;
+  }
+  `;
 
 export const RightContainer = styled.div`
   flex: 20%;
   display: flex;
   justify-content: flex-end;
   padding-right: 80px;
-`;
+  `;
 export const Logo = styled.img`
   max-width: 180px;
   height: auto;
-`;
+  `;
 
 export const NavbarInnerContainer = styled.div`
   width: 100%;
   height: 80px;
   display: flex;
-`;
+  `;
 
 export const NavbarLink = styled(Link)`
   display: flex;
@@ -42,7 +46,7 @@ export const NavbarLink = styled(Link)`
   font-family: Arial, Helvetica, sans-serif;
   text-decoration: none;
   margin: 10px;
-
+  
   img {
     /* 로고 */
     max-width: 180px;
@@ -51,20 +55,19 @@ export const NavbarLink = styled(Link)`
   .searchForm {
     /* 검색창 */
     display: flex;
-    width: 700px;
+    width:100%;
     height: 40px;
-    margin-left: 40px;
+   // margin-left: 40px;
     padding: 0 15px;
     cursor: pointer;
   }
-
+  
   .loginBtn,
   .signupBtn {
     /* 로그인,회원가입 */
     cursor: pointer;
     border-radius: 5px;
     border: solid 1px rgb(1, 153, 255);
-
     background-color: rgb(1, 153, 255);
     font-size: 1rem;
     height: 46px;
@@ -82,7 +85,7 @@ export const NavbarLink = styled(Link)`
   @media (max-width: 700px) {
     display: none;
   }
-`;
+  `;
 
 // 여기서 부터, 사이즈가 작아졌을때 나타나는 버거메뉴 -----
 export const OpenLinksButton = styled.button`
@@ -96,14 +99,14 @@ export const OpenLinksButton = styled.button`
   @media (min-width: 700px) {
     display: none;
   }
-`;
+  `;
 export const NavbarLinkExtended = styled(Link)`
   color: white;
   font-size: x-large;
   font-family: Arial, Helvetica, sans-serif;
   text-decoration: none;
   margin: 10px;
-`;
+  `;
 
 export const NavbarExtendedContainer = styled.div`
   display: flex;
